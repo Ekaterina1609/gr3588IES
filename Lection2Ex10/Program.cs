34f8fd2700656ebae8614123f54f132e3da58951
EIVANOVA1609 <eivanova1609@yandex-team.ru>
2023-07-05 12:37:37 +0300

    работа с массивами

---
+++ Lection2Ex10/Program.cs
@@ -0,0 +1,19 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = { 1, 72, 3, 4, 72, 6 };
+        int n = array.Length;
+        int find = 72;
+        int index = 0;
+        while (index < n)
+        {
+            if (array[index] == find)
+                {Console.WriteLine(index);
+               break;
+                }
+            index++;
+        }
+        Console.WriteLine("end");
+    }
+}
